69215c726ba08f499d27fb23447da4c598f4e6d9
Luis Luna <luislunandroid@gmail.com>
2022-08-15 19:02:43 -0400

    version 5.0

---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const JumboTron = ()=> {
+    return (
+        <div className="jumbotron">
+  <h1 className="display-4">Evangelion</h1>
+        <p className="lead">Los Evangelion (エヴァンゲリオン), abreviado EVAs, son mechas de combate pilotados por los Children Rei Ayanami, Asuka Langley Soryu y Shinji Ikari. Fueron diseñados por Ikuto Yamashita y el director de la serie Hideaki Anno.
+        </p>
+        <p>
+        Aunque aparentan ser robots humanoides gigantes, los EVAs son seres biológicos potenciados cibernética y mecánicamente. Fueron creados por la organización GEHIRN y su sucesora NERV para defender Tokio-3 de la amenaza de los Ángeles e impedir el evento catastrófico conocido como Tercer Impacto. Sin embargo, existe un motivo oculto que justifica su existencia.
+        </p>
+ 
+ 
+    <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+
+</div>
+
+
+
+    </div>
+    )
+   
+
+
+   
+
+}
