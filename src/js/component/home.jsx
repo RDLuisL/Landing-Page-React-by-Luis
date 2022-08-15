94c36eb54f91d951e929ea3b5ba0d8880b86f33f
Luis Luna <luislunandroid@gmail.com>
2022-08-15 18:43:08 -0400

    version 4

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,34 @@
 import React from "react";
+//esto es importar una dependencia de React para que se reconozcan los PropTypes
+import PropTypes from "prop-types";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Card = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="card h-25 pr-3 mt-4" style={{width: "20rem"}}>
+			<img
+			style={{width: "auto", height: "25rem", objectFit: "cover"}}
+			className = "card-img-top"
+			src={props.img}
+			alt="Card imagen"
+			/>
+			<div className="car-body">
+				<h3 className="card-title">{props.title}</h3>
+				<p classNAme="card-text"></p>
+				<a href="#" className="btn btn-primary"> {props.buttonText}</a>
+			</div>
 		</div>
 	);
 };
+//Comentarios de como son las entradas de la constante Card
+Card.propoTypes = {
+	img: PropTypes.string.isRequired,
+	title: PropTypes.string,
+	text: PropTypes.string,
+	buttonText: PropTypes.string
+}
 
-export default Home;
+export default Card;
