c5c4d7afbc3ad4442577dcef49b6feb72e02e2a8
Luis Luna <luislunandroid@gmail.com>
2022-08-16 17:10:29 -0400

    version 6.2

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -2,10 +2,9 @@ import React from "react";
 //esto es importar una dependencia de React para que se reconozcan los PropTypes
 import PropTypes from "prop-types";
 
-const Footer ()=> {
+const Footer = ()=> {
     return (
-
-
+        <div className="text-bg-dark p-3"> Copywright @ Luis Luna 2022</div>;
     )
 }
 
